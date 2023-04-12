d96ab67d22fe7186ec5f96b908b1f93bac3d54b4
apexdev <mwangi.em37@gmail.com>
2023-04-12 13:02:27 +0300

    fix: Modify add task logic
    Modify add task logic to add newly added and persisted task into the tasks data property to dynamically update tasks and make visible.

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -19,13 +19,14 @@ export default {
     data() {
         return {
             tasks: [],
+            BASE_URI: 'http://localhost:5000',
         };
     },
     methods: {
         async deleteTask(taskId) {
             console.log(taskId);
             if (confirm("Are you sure you want to delete?")) {
-                const response = await fetch(`http://localhost:5000/tasks/${taskId}`, {
+                const response = await fetch(`${this.BASE_URI}/tasks/${taskId}`, {
                     method: "DELETE",
                 });
 
@@ -39,7 +40,7 @@ export default {
 
             const taskToToggle = await this.fetchTask(taskId);
             const updatedTask = { ...taskToToggle, reminder: !taskToToggle.reminder };
-            const response = await fetch(`http://localhost:5000/tasks/${taskId}`, {
+            const response = await fetch(`${this.BASE_URI}/tasks/${taskId}`, {
                 method: "PUT",
                 headers: { "Content-type": "application/json" },
                 body: JSON.stringify(updatedTask),
@@ -54,26 +55,26 @@ export default {
             this.showAddTask = !this.showAddTask;
         },
         async fetchTasks() {
-            const response = await fetch("http://localhost:5000/tasks");
+            const response = await fetch(`${this.BASE_URI}/tasks`);
             const data = await response.json();
 
             return data;
         },
         async fetchTask(taskId) {
-            const response = await fetch(`http://localhost:5000/tasks/${taskId}`);
+            const response = await fetch(`${this.BASE_URI}/tasks/${taskId}`);
             const data = await response.json();
 
             return data;
         },
         async addTask(newTask) {
-            const response = await fetch("http://localhost:5000/tasks", {
+            const response = await fetch(`${this.BASE_URI}/tasks`, {
                 method: "POST",
                 headers: { "Content-type": "application/json" },
                 body: JSON.stringify(newTask),
             });
 
             const data = await response.json();
-            this.tasks = [...this.task, data];
+            this.tasks = [...this.tasks, data];
         },
     },
     async created() {
