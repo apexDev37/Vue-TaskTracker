e46c20b27b2889552e1f02631c5b3113089d2d94
apexdev <mwangi.em37@gmail.com>
2023-04-25 12:23:19 +0300

    chore: Add alpine script
    Add alpine launch script to run the application - stand up both frontend and backend servers.

---
+++ scripts/launch-app-alpine.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+# Start the vue front-end
+npm run serve &
+
+# Start the json-server backend
+npm run backend &
+
+# Wait for any process to exit
+wait -n
+
+# Exit with status of process that exited first
+exit $?
